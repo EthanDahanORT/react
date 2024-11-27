a0944e84295a720f32b9edb074bd8affeda17e1c
EthanD <ethan219.ed@gmail.com>
2024-11-27 11:46:34 +0100

    push

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GitLab CI
+          Learn GitLab CI okk
         </a>
       </header>
     </div>
